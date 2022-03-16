ede80c2b150f2c7de847483e94d8a5cd93dc1dc2
OneS18 <diablo201888@gmail.com>
2022-03-16 18:56:40 +0200

    done with frontend

--- src/app/components/view-contact/view-contact.component.ts
+++ src/app/components/view-contact/view-contact.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ParamMap } from '@angular/router';
+import { ContactService } from 'src/app/services/contact.service';
+import { IContact } from 'src/app/models/IContact';
 
 @Component({
   selector: 'app-view-contact',
   templateUrl: './view-contact.component.html',
-  styleUrls: ['./view-contact.component.css']
+  styleUrls: ['./view-contact.component.css'],
 })
 export class ViewContactComponent implements OnInit {
+  public loading: boolean = false;
+  public contactId: IContact | null | string = null;
+  
+  public contact: IContact = {} as IContact;
+  public errorMessage: string | null = null;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private contactService: ContactService
+  ) {}
+
+  // ngOnInit(): void {
+  //   this.activatedRoute.paramMap.subscribe((param: ParamMap) => {
+  //     this.contactId = param.get('contactId');
+  //   });
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param: ParamMap) => {
+      this.contactId = param.get('contactId');
+    });
+    if (this.contactId) {
+      this.loading = true;
+      this.contactService.getContact(this.contactId).subscribe(
+        (data: IContact) => {
+          this.contact = data;
+          this.loading = false;
+        },
+        (error) => {
+          this.errorMessage = error;
+          this.loading = false;
+        }
+      );
+    }
   }
-
 }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,18 +1,88 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IContact } from '../models/IContact';
+import { IGroup } from '../models/IGroup';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContactService {
-  private serverUrl: string = `http://localhost:9000/`; //json server URL
+  private serverUrl: string = `http://localhost:9000`; //json server URL
 
   constructor(private httpClient: HttpClient) {}
 
+  //get all contacts
   public getAllContacts(): Observable<IContact[]> {
     let dataURL: string = `${this.serverUrl}/contacts`;
-    return this.httpClient.get<IContact[]>(dataURL).pipe();
+    return this.httpClient
+      .get<IContact[]>(dataURL)
+      .pipe(catchError(this.handleError));
+  }
+
+  //Get Single Contact
+  public getContact(contactId: IContact): Observable<IContact> {
+    let dataURL: string = `${this.serverUrl}/contacts/${contactId}`;
+    return this.httpClient
+      .get<IContact>(dataURL)
+      .pipe(catchError(this.handleError));
+  }
+
+  //Create  a contact
+  public createContact(contact: IContact): Observable<IContact> {
+    let dataURL: string = `${this.serverUrl}/contacts`;
+    return this.httpClient
+      .post<IContact>(dataURL, contact)
+      .pipe(catchError(this.handleError));
+  }
+
+  //update contact
+  public updateContact(
+    contact: IContact,
+    contactId: IContact
+  ): Observable<IContact> {
+    let dataURL: string = `${this.serverUrl}/contacts/${contactId}`;
+    return this.httpClient
+      .put<IContact>(dataURL, contact)
+      .pipe(catchError(this.handleError));
+  }
+
+  //Delete contacts
+  public deleteContact(contact: IContact, contactId: string): Observable<{}> {
+    let dataURL: string = `${this.serverUrl}/contacts/${contactId}`;
+    return this.httpClient
+      .delete<{}>(dataURL)
+      .pipe(catchError(this.handleError));
+  }
+
+  //get All groups
+  public getAllGroups(): Observable<IGroup[]> {
+    let dataURL: string = `${this.serverUrl}/groups`;
+    return this.httpClient
+      .get<IGroup[]>(dataURL)
+      .pipe(catchError(this.handleError));
+  }
+
+  //get single group
+  public getGroup(contact: IContact): Observable<IGroup> {
+    let dataURL: string = `${this.serverUrl}/groups/${contact.groupId}`;
+    return this.httpClient
+      .get<IGroup>(dataURL)
+      .pipe(catchError(this.handleError));
+  }
+
+  //error handling
+  public handleError(error: HttpErrorResponse) {
+    let errorMessage: string = '';
+    if (error.error instanceof ErrorEvent) {
+      //client error
+      errorMessage = `Error : ${error.error.message}`;
+    } else {
+      //server error
+      errorMessage = `Status : ${error.status} \n Message: ${error.message}`;
+    }
+    return throwError(errorMessage);
   }
 }
